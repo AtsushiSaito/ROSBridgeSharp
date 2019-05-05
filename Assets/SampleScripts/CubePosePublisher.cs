dbefc0128207db82add7cc365cc9ec281303bd62
AtsushiSaito <atsushi31100854@gmail.com>
2019-05-05 21:10:42 +0900

    Add CubePosePublisher

---
+++ Assets/SampleScripts/CubePosePublisher.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RBS;
+using RBS.Messages;
+
+public class CubePosePublisher : MonoBehaviour
+{
+    private RBPublisher<RBS.Messages.geometry_msgs.Pose> pose_pub;
+    public GameObject cube;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        pose_pub = new RBPublisher<RBS.Messages.geometry_msgs.Pose>("/pose");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(RBSocket.Instance.IsConnected);
+        if (RBSocket.Instance.IsConnected) {
+
+            RBS.Messages.geometry_msgs.Pose pose = new RBS.Messages.geometry_msgs.Pose();
+            pose.position.x = cube.transform.position.x;
+            pose.position.y = cube.transform.position.y;
+            pose.position.z = cube.transform.position.z;
+            pose_pub.publish(pose);
+        }
+    }
+}
