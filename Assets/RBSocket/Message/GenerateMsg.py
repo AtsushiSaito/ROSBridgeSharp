e36b179225d701ba09f3ac017b098832fecf3393
AtsushiSaito <atsushi31100854@gmail.com>
2019-05-06 07:46:43 +0900

    Brashup GenerateMsg.py

--- Assets/RBSocket/Message/GenerateMsg.py
+++ Assets/RBSocket/Message/GenerateMsg.py
@@ -1,160 +1,252 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import subprocess
 import sys
 args = sys.argv
 
-cmd = "rosmsg show " + args[1]
 
-namespace = args[1].rsplit("/",1)[0]
-msg_name = args[1].rsplit("/",1)[1]
+class CHClassGenerate():
+    def __init__(self):
+        self.CSharpTypeName = ["bool", "string", "Time", "int", "uint", "int",
+                               "uint", "int", "uint", "int", "uint", "float",
+                               "double", "Duration", "byte", "char"]
 
-if msg_name == "Time" or msg_name == "Duration":
-    print("not support.")
-    exit()
+        self.ROSTypeName = ["bool", "string", "time", "int8", "uint8", "int16",
+                            "uint16", "int32", "uint32", "int64", "uint64", "float32",
+                            "float64", "duration", "byte", "char"]
 
-process = subprocess.Popen(cmd, stdout=subprocess.PIPE,shell=True).communicate()[0]
+        self.CSharpInitialValues = ["false", "\"\"", "new Time()", "0", "0", "0", "0",
+                                    "0", "0", "0", "0", "0.0f", "0.0f", "new Duration()", "0", "\'\0\'"]
 
-files = process.split("\n")
-process = ""
-in_type_namespace = []
+        self.set_msg_name(args[1])
+        self.CheckMessgage()
+        self.CommandMsgShowRun()
+        self.CommandRowData_to_NoNestMsgData()
+        self.ParseTypeNameSpace()
+        self.SplitTypeAndName()
+        self.GenerateMsgText()
+        self.Save()
 
-for i in range(len(files) - 2):
-    if files[i].count(" ") < 2:
-        if files[i].count("/") == 0:
-            process += files[i] + "\n"
-            in_type_namespace.append("")
-        else:
-            process += files[i].rsplit("/")[1] + "\n"
-            in_type_namespace.append(files[i].rsplit("/")[0])
+    # メッセージの名前とネームスペースを分離
+    def set_msg_name(self, msg_arg):
+        self.msg = msg_arg
+        self.msg_namespace = msg_arg.rsplit("/", 1)[0]
+        self.msg_name = msg_arg.rsplit("/", 1)[1]
 
-splited_data = process.split()
+    # TimeとDurationは除外する。
+    def CheckMessgage(self):
+        if self.msg_name == "Time" or self.msg_name == "Duration":
+            exit()
 
-c_sharp_type = ["bool", "string", "Time", "int",  "uint",  "int",   "uint",   "int",   "uint",   "int",   "uint",   "float",   "double",  "Duration", "byte", "char"]
-ros_type =     ["bool", "string", "time", "int8", "uint8", "int16", "uint16", "int32", "uint32", "int64", "uint64", "float32", "float64", "duration", "byte", "char"]
-initial_data = ["false", "\"\"", "new Time()", "0", "0", "0", "0", "0", "0", "0", "0", "0.0f", "0.0f", "new Duration()", "0", "\'\0\'"]
+    def CommandMsgShowRun(self):
+        # rosmsg showコマンドを実行してメッセージ情報を受取る
+        self.CommandRowData = subprocess.Popen(
+            "rosmsg show " + self.msg, stdout=subprocess.PIPE, shell=True).communicate()[0]
 
-in_type_list = []
-in_type_isArray = []
-in_name_list = []
-in_name_IsInited = []
-in_name_InitedValue = []
-in_type_custom = []
+    def CommandRowData_to_NoNestMsgData(self):
+        self.NoNestMsgData = ""
+        data = self.CommandRowData.split("\n")  # 改行で分割
+        for i in range(len(data)):
+            # データが空の場合はコンティニュー
+            if data[i] == "":
+                continue
+            # スペースが2個以上含まれる場合はネストと判断してスキップ
+            if data[i].count(" ") < 2:
+                self.NoNestMsgData += data[i]
+                if not i == len(data) - 3:
+                    self.NoNestMsgData += "\n"
 
-in_name_IsArrayInited = []
-in_name_ArrayInitedValue = []
-
-for i in range(len(splited_data)):
-    data = splited_data[i]
-    if(0 == i % 2):
-        isArray = data.find('[')
-        isHit = False
-        isArrayInited = False
-        if(isArray == -1):
-            in_type_isArray.append(False)
-            for y in range(len(ros_type)):
-                if ros_type[y] == str(data):
-                    in_type_list.append(c_sharp_type[y])
-                    isHit = True
-                    in_type_custom.append(False)
-            if not isHit:
-                in_type_list.append(str(data))
-                in_type_custom.append(True)
-            in_name_IsArrayInited.append(False)
-            in_name_ArrayInitedValue.append("")
-        else:
-            in_value_buf = ""
-            if data[isArray + 1] == "]":
-                in_name_IsArrayInited.append(False)
-                in_name_ArrayInitedValue.append("")
+    def ParseTypeNameSpace(self):
+        self.ParseMsgData = ""
+        self.TypeNameSpaces = []
+        data = self.NoNestMsgData.split("\n")  # 改行で分割
+        for i in range(len(data)):
+            if data[i].count("/") == 0:
+                self.TypeNameSpaces.append("")
+                self.ParseMsgData += data[i] + "\n"
             else:
-                end_index = data.find(']')
-                in_name_IsArrayInited.append(True)
-                isArrayInited = True
-                in_value_buf = data[isArray+1:end_index]
-                in_name_ArrayInitedValue.append(in_value_buf)
-            in_type_isArray.append(True)
+                self.TypeNameSpaces.append(data[i].rsplit("/")[0])
+                self.ParseMsgData += data[i].rsplit("/")[1] + "\n"
 
-            for y in range(len(ros_type)):
-                if isArrayInited:
-                    end_index = data.find(']')
-                    if (ros_type[y]+"[" + in_value_buf +"]") == (str(data)):
-                        in_type_list.append(c_sharp_type[y])
-                        isHit = True
-                        in_type_custom.append(False)
+    def SplitTypeAndName(self):
+        # 型リスト
+        self.TypeList = []
+        # 変数名リスト
+        self.NameList = []
+        # ネームスペースがあるかどうか
+        self.IsCustonNameSpace = []
+        # 初期値があるかどうか
+        self.IsInited = []
+        # 初期値の値
+        self.InitedValues = []
+        # 配列であるかどうか
+        self.IsArray = []
+
+        # 配列に初期値があるかどうか
+        self.IsArrayInited = []
+        # 配列であった場合に、初期値があるかないか
+        self.ArrayInitialValues = []
+
+        datas = self.ParseMsgData.split()
+
+        # 分割した個数分だけ回す
+        for i in range(len(datas)):
+            # 偶数の場合は型
+            if(0 == i % 2):
+                isArray = datas[i].find('[')
+                hasOrinalType = False
+                # 配列でない場合
+                if(isArray == -1):
+                    # ------------------------------------
+                    # 配列ではないので
+                    self.IsArray.append(False)
+                    # 配列でなく、初期値もないので
+                    self.ArrayInitialValues.append("")
+                    # ------------------------------------
+                    for j in range(len(self.ROSTypeName)):
+                        if self.ROSTypeName[j] == str(datas[i]):
+                            self.TypeList.append(self.CSharpTypeName[j])
+                            # オリジナルタイプである
+                            hasOrinalType = True
+                            # ------------------------------------
+                            # カスタムネームスペースではない
+                            self.IsCustonNameSpace.append(False)
+                            # ------------------------------------
+                    # オリジナルタイプではない場合
+                    if not hasOrinalType:
+                        # ------------------------------------
+                        self.TypeList.append(str(datas[i]))
+                        self.IsCustonNameSpace.append(True)
+                        # ------------------------------------
+                    self.IsArrayInited.append(False)
+                # 配列の場合
                 else:
-                    if (ros_type[y]+"[]") == (str(data)):
-                        in_type_list.append(c_sharp_type[y])
-                        isHit = True
-                        in_type_custom.append(False)
-            if not isHit:
-                in_type_list.append(str(data)[:-2])
-                in_type_custom.append(True)
-    else:
-        InitedIndex = data.find('=')
-        if InitedIndex == -1:
-            in_name_list.append(data)
-            in_name_IsInited.append(False)
-            in_name_InitedValue.append("")
-        else:
-            cut_index = len(data) - InitedIndex
-            in_name_list.append(data[:-cut_index])
-            in_name_IsInited.append(True)
-            in_name_InitedValue.append(data[InitedIndex + 1:])
-
-output_class = "using System;\n\nnamespace RBS.Messages."+ namespace + "\n{\n"
-output_class += "    [System.Serializable]\n"
-output_class += "    public class " + msg_name + " : ExtendMessage\n    {\n"
-
-for i in range(len(in_type_list)):
-    if in_type_custom[i]:
-        if in_type_isArray[i]:
-            output_class += "        public " + "RBS.Messages." + in_type_namespace[i] + "." + in_type_list[i] + "[] " + in_name_list[i] + ";\n"
-        else:
-            output_class += "        public " + "RBS.Messages." + in_type_namespace[i] + "." + in_type_list[i] + " " + in_name_list[i] + ";\n"
-    else:
-        if in_type_isArray[i]:
-            output_class += "        public "+ in_type_list[i] + "[] " + in_name_list[i] + ";\n"
-        else:
-            output_class += "        public "+ in_type_list[i] + " " + in_name_list[i] + ";\n"
-output_class += "        public override string Type() { return \"" + args[1] + "\"; }\n"
-
-output_class += "        public " + msg_name + "()\n        {\n"
-for i in range(len(in_type_list)):
-    isHit = False
-    for y in range(len(c_sharp_type)):
-        if c_sharp_type[y] == in_type_list[i] and not isHit:
-            isHit = True
-            if in_type_isArray[i]:
-                if not in_name_IsInited[i]:
-                    if in_name_IsArrayInited[i]:
-                        output_class += "            " + in_name_list[i] + " = new " + in_type_list[i] + "[" + in_name_ArrayInitedValue[i] + "];\n"
+                    # 配列なので
+                    self.IsArray.append(True)
+                    # 配列に初期値
+                    hasArrayValue = False
+                    # 配列の初期値
+                    ArrayValue = ""
+                    # 括弧が次にある場合
+                    if datas[i][isArray + 1] == "]":
+                        self.IsArrayInited.append(False)
+                        self.ArrayInitialValues.append("")
+                    # 初期値ある場合
                     else:
-                        output_class += "            " + in_name_list[i] + " = new " + in_type_list[i] + "[0];\n"
-                else:
-                    output_class += "            " + in_name_list[i] + " = " + in_name_InitedValue[i] + ";\n"
-            else:
-                if not in_name_IsInited[i]:
-                    output_class += "            " + in_name_list[i] + " = " + initial_data[y] + ";\n"
-                else:
-                    output_class += "            " + in_name_list[i] + " = " + in_name_InitedValue[i] + ";\n"
-    if not isHit:
-        if in_type_isArray[i]:
-            if not in_name_IsInited[i]:
-                if in_name_IsArrayInited[i]:
-                    output_class += "            " + in_name_list[i] + " = new " + "RBS.Messages." + in_type_namespace[i] + "." + in_type_list[i] + "[" + in_name_ArrayInitedValue[i] + "];\n"
-                else:
-                    output_class += "            " + in_name_list[i] + " = new " + "RBS.Messages." + in_type_namespace[i] + "." + in_type_list[i] + "[0];\n"
-            else:
-                output_class += "            " + in_name_list[i] + " = " + in_name_InitedValue[i]  + ";\n"
-        else:
-            if not in_name_IsInited[i]:
-                output_class += "            " + in_name_list[i] + " = " + "new " + "RBS.Messages." + in_type_namespace[i] + "." + in_type_list[i] + "();\n"
-            else:
-                output_class += "            " + in_name_list[i] + " = " + in_name_InitedValue[i] + ";\n"
+                        self.IsArrayInited.append(True)
+                        # 括弧の終わりのインデックス番号を調べる
+                        end_index = datas[i].find(']')
+                        hasArrayValue = True
+                        ArrayValue = datas[i][isArray + 1: end_index]
+                        self.ArrayInitialValues.append(ArrayValue)
 
-output_class += "        }\n    }\n}"
+                    for j in range(len(self.ROSTypeName)):
+                        if hasArrayValue:
+                            end_index = datas[i].find(']')
+                            if (self.ROSTypeName[j] + "[" + ArrayValue + "]") == (str(datas[i])):
+                                self.TypeList.append(self.CSharpTypeName[j])
+                                hasOrinalType = True
+                                self.IsCustonNameSpace.append(False)
+                        else:
+                            if (self.ROSTypeName[j] + "[]") == (str(datas[i])):
+                                self.TypeList.append(self.CSharpTypeName[j])
+                                hasOrinalType = True
+                                self.IsCustonNameSpace.append(False)
+                    if not hasOrinalType:
+                        self.TypeList.append(str(datas[i])[:-2])
+                        self.IsCustonNameSpace.append(True)
+            else:
+                equal_index = datas[i].find('=')
+                if equal_index == -1:
+                    self.NameList.append(datas[i])
+                    self.IsInited.append(False)
+                    self.InitedValues.append("")
+                else:
+                    cut_index = len(datas[i]) - equal_index
+                    self.NameList.append(datas[i][:-cut_index])
+                    self.IsInited.append(True)
+                    self.InitedValues.append(datas[i][equal_index + 1:])
 
-f = open(msg_name +".cs", "w")
-f.write(output_class)
-f.close()
+    def GS(self, n):
+        space = ""
+        for _ in range(n):
+            space += " "
+        return space
 
-print(output_class)
+    def GenerateMsgText(self):
+        text = ""
+        text += "using System;\n\n"
+        text += "namespace RBS.Messages." + self.msg_namespace + "\n{\n"
+        text += self.GS(4) + "[System.Serializable]\n"
+        text += self.GS(4) + "public class " + self.msg_name
+        text += " : ExtendMessage\n"
+        text += self.GS(4) + "{\n"
+
+        # クラス定義のところまで
+        for i in range(len(self.TypeList)):
+            # ネームスペースが存在する場合
+            if self.IsCustonNameSpace[i]:
+                text += self.GS(8) + "public " + "RBS.Messages."
+                text += self.TypeNameSpaces[i] + "." + self.TypeList[i]
+            else:
+                text += self.GS(8) + "public " + self.TypeList[i]
+            if self.IsArray[i]:
+                text += "[] "
+            else:
+                text += " "
+            text += self.NameList[i] + ";\n"
+
+        text += self.GS(8) + "public override string Type() { return \""
+        text += self.msg + "\"; }\n"
+        text += self.GS(8) + "public " + self.msg_name + "()\n"
+        text += self.GS(8) + "{\n"
+
+        # 初期化の箇所
+        for i in range(len(self.TypeList)):
+            isOrignal = False
+            text += self.GS(12) + self.NameList[i] + " = "
+            for j in range(len(self.CSharpTypeName)):
+                if self.CSharpTypeName[j] == self.TypeList[i] and not isOrignal:
+                    isOrignal = True
+                    if self.IsArray[i]:
+                        if not self.IsInited[i]:
+                            text += "new " + self.TypeList[i] + "["
+                            if self.IsArrayInited[i]:
+                                text += self.ArrayInitialValues[i] + "];\n"
+                            else:
+                                text += "0];\n"
+                        else:
+                            text += self.InitedValues[i] + ";\n"
+                    else:
+                        if not self.IsInited[i]:
+                            text += self.CSharpInitialValues[j] + ";\n"
+                        else:
+                            text += self.InitedValues[i] + ";\n"
+            if not isOrignal:
+                if self.IsArray[i]:
+                    if self.IsArrayInited[i]:
+                        text += "new " + "RBS.Messages."
+                        text += self.TypeNameSpaces[i] + "."
+                        text += self.TypeList[i] + "["
+                        text += self.ArrayInitialValues[i] + "];\n"
+                    else:
+                        text += "new " + "RBS.Messages."
+                        text += self.TypeNameSpaces[i] + "."
+                        text += self.TypeList[i] + "[0];\n"
+                else:
+                    text += "new " + "RBS.Messages."
+                    text += self.TypeNameSpaces[i] + "."
+                    text += self.TypeList[i] + "();\n"
+        text += "        }\n    }\n}"
+        self.maked_text = text
+
+    def Save(self):
+        file = open(self.msg_name + ".cs", "w")
+        file.write(self.maked_text)
+        file.close()
+        print(self.maked_text)
+
+
+if __name__ == '__main__':
+    CHClassGenerate()
