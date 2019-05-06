7cab76b0b9f40ead69f189d557ca105f70379e4e
AtsushiSaito <atsushi31100854@gmail.com>
2019-05-06 18:58:37 +0900

    Add GetTimeServiceAsync

---
+++ Assets/SampleScripts/GetTimeServiceAsync.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RBS;
+using RBS.Messages;
+
+public class GetTimeServiceAsync : MonoBehaviour
+{
+    private RBServiceAsyncClient<RBS.Messages.rosapi.GetTimeRequest, RBS.Messages.rosapi.GetTimeResponse> service_client;
+    private bool isRequested;
+
+    void Awake()
+    {
+        isRequested = false;
+        service_client = new RBServiceAsyncClient<RBS.Messages.rosapi.GetTimeRequest, RBS.Messages.rosapi.GetTimeResponse>("/rosapi/get_time", service_callback);
+    }
+
+    void service_callback(RBS.Messages.rosapi.GetTimeResponse response)
+    {
+        Debug.Log(response.time.nsecs);
+        isRequested = false;
+    }
+
+    void Update()
+    {
+        if (RBSocket.Instance.IsConnected && !isRequested)
+        {
+            RBS.Messages.rosapi.GetTimeRequest request_data = new RBS.Messages.rosapi.GetTimeRequest();
+            RBS.Messages.rosapi.GetTimeResponse response_data = new RBS.Messages.rosapi.GetTimeResponse();
+            service_client.Call(ref request_data, ref response_data);
+            isRequested = true;
+        }
+    }
+}
