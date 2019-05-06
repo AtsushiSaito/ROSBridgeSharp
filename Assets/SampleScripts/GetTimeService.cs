d3ab242f41af410d07b8cedf2407ac2668dea1e6
AtsushiSaito <atsushi31100854@gmail.com>
2019-05-06 17:52:58 +0900

    Add GetTimeService

---
+++ Assets/SampleScripts/GetTimeService.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RBS;
+using RBS.Messages;
+
+public class GetTimeService : MonoBehaviour
+{
+    private RBServiceClient<RBS.Messages.rosapi.GetTimeRequest, RBS.Messages.rosapi.GetTimeResponse> service_client;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        service_client = new RBServiceClient<RBS.Messages.rosapi.GetTimeRequest, RBS.Messages.rosapi.GetTimeResponse>("/rosapi/get_time");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log(RBSocket.Instance.IsConnected);
+        if (RBSocket.Instance.IsConnected)
+        {
+            RBS.Messages.rosapi.GetTimeRequest request_data = new RBS.Messages.rosapi.GetTimeRequest();
+            RBS.Messages.rosapi.GetTimeResponse response_data = new RBS.Messages.rosapi.GetTimeResponse();
+            service_client.Call(ref request_data, ref response_data);
+            Debug.Log(response_data.time.nsecs);
+        }
+    }
+}
